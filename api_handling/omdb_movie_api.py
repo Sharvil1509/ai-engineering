c9b4e3d92a935fd73d9aef635ed1bdd86f9dee60
Sharvil Nikam <sharvilgnikam15@gmail.com>
2026-06-26 16:03:42 +0530

    Added Search Multiple Movies

--- api_handling/omdb_movie_api.py
+++ api_handling/omdb_movie_api.py
@@ -4,7 +4,7 @@ def get_movie(movie):
     try:
         url = "https://www.omdbapi.com/"
         params = {
-            "t": movie,
+            "s": movie,
             "apikey": "171fbd6"
         }
     except Exception:
@@ -13,20 +13,51 @@ def get_movie(movie):
     response = requests.get(url, params=params)
     return response.json()
 
+def get_movie_details(imdb_id):
+    url = "https://www.omdbapi.com/"
+    params = {
+        "i": imdb_id,
+        "apikey": "171fbd6"
+    }
+
+    response = requests.get(url, params=params)
+    return response.json()
 
 def main():
-    movie = input("Enter Movie Title: ")
+    query = input("Search movie: ")
 
-    data = get_movie(movie)
+    data = get_movie(query)
 
-    if data is None:
-        print("Network error")
-    elif data["Response"] == "True":
-        print(f"Title      : {data['Title']}")
-        print(f"Released   : {data['Released']}")
-        print(f"Director   : {data['Director']}")
+    if data["Response"] == "True":
+
+        movies = data["Search"]
+
+        # show list with numbers
+        for i, movie in enumerate(movies, start=1):
+            print(f"{i}. {movie['Title']} ({movie['Year']})")
+
+        # user selection
+        choice = int(input("\nEnter choice number: "))
+
+        # convert choice → index
+        selected_movie = movies[choice - 1]
+
+        # extract imdbID
+        imdb_id = selected_movie["imdbID"]
+
+        print("\nSelected IMDb ID:", imdb_id)
+
+        # fetch full details
+        details = get_movie_details(imdb_id)
+
+        print("\n--- FULL MOVIE DETAILS ---")
+        print("Title:", details["Title"])
+        print("Year:", details["Year"])
+        print("Director:", details["Director"])
+        print("Plot:", details["Plot"])
+
     else:
-        print(data["Error"])
+        print("Error:", data["Error"])
 
 
 if __name__ == "__main__":
