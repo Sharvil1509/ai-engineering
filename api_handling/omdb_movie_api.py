0711bb0705cda7f55eed1b50a81e3b88751cf9ff
Sharvil Nikam <sharvilgnikam15@gmail.com>
2026-06-26 15:03:51 +0530

    OMDb API project - basic movie fetch working

--- api_handling/omdb_movie_api.py
+++ api_handling/omdb_movie_api.py
@@ -1,11 +1,14 @@
 import requests
 
 def get_movie(movie):
-    url = "https://www.omdbapi.com/"
-    params = {
-        "t": movie,
-        "apikey": "171fbd6"
-    }
+    try:
+        url = "https://www.omdbapi.com/"
+        params = {
+            "t": movie,
+            "apikey": "171fbd6"
+        }
+    except Exception:
+         return None    
 
     response = requests.get(url, params=params)
     return response.json()
@@ -16,7 +19,9 @@ def main():
 
     data = get_movie(movie)
 
-    if data["Response"] == "True":
+    if data is None:
+        print("Network error")
+    elif data["Response"] == "True":
         print(f"Title      : {data['Title']}")
         print(f"Released   : {data['Released']}")
         print(f"Director   : {data['Director']}")
