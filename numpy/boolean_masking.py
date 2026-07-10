764dd8759e412e9e754f9f023199e4b7fecdaa84
Sharvil Nikam <sharvilgnikam15@gmail.com>
2026-07-10 18:38:53 +0530

    Add NumPy boolean masking and matrix operations

---
+++ numpy/boolean_masking.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print(numbers > 25)
+print(numbers[numbers > 25])
---
+++ numpy/matrix_operations.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+A = np.array([
+    [1, 2],
+    [3, 4]
+])
+
+B = np.array([
+    [5, 6],
+    [7, 8]
+])
+
+print(A)
+print(A.T)  # Transpose
+print(A @ B)  # Matrix Multiplication
